3ef46abbfceeccd04d7d129439808ba12949a82a
AranganathanPrakash <114086134+AranganathanPrakash@users.noreply.github.com>
2024-05-15 10:54:24 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    sh'chmod +x build.sh'
+    sh'./build.sh'
+    docker login -u ar8888 -p $DOCKER_PASSWORD
+    docker tag react-nginx ar8888/dev
+    docker push ar8888/dev
+
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
+    sh'chmod +x build.sh'
+    sh'./build.sh'
+    docker login -u ar8888 -p $DOCKER_PASSWORD
+    docker tag react-nginx ar8888/prod 
+    docker push ar8888/prod
+fi
