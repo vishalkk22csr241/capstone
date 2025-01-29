6054e220e60940a742fb80040ae7f8ed26d711d4
AranganathanPrakash <114086134+AranganathanPrakash@users.noreply.github.com>
2025-01-29 15:04:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker push ar8888/dev
+    sh 'docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w'
+    sh 'docker push ar8888/dev'
     
