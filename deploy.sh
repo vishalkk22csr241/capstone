578e811d0a8e54be8d3229d1850fec970dd3f224
VISHAL K K <vishalkarthi01@gmail.com>
2025-03-20 08:58:40 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@
     docker login -u kkvishal -p dckr_pat_sQFOXY_hbjQ1lCVFhTp4qTokwhI
     docker tag test1 kkvishal/devops
     docker push kkvishal/devops
-    # docker-compose up -d
+    docker-compose up -d
     
