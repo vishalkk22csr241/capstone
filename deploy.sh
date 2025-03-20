38ae793831cac1123f93d3add79cad042219aec3
VISHAL K K <vishalkarthi01@gmail.com>
2025-03-20 08:12:32 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u kkvishal -p dckr_pat_sQFOXY_hbjQ1lCVFhTp4qTokwhI
+    docker tag test1 kkvishal/devops
+    docker push kkvishal/devops
+    # docker-compose up -d
     
